95de0cb34add987197a6fb1de9881c411ee2ab52
Rehan Akbar <rehan.akbar@paismo.com>
2024-10-25 14:07:08 +0500

    Few Changes Made

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <AddToDo myProps={toDo}/>
+      <AddToDo props={toDo}/>
     </>
   )
 }
--- src/Components/AddToDo.jsx
+++ src/Components/AddToDo.jsx
@@ -2,56 +2,48 @@ import { observer } from "mobx-react";
 import React, { useState } from "react";
 import "./styles.css";
 
-const AddToDo = observer (({myProps}) => {
-  const [todo, setTodo] = useState('');
+const AddToDo = observer(({ props }) => {
+  const [todo, setTodo] = useState("");
 
   const addTodo = () => {
-    if (todo !== ""){
-    myProps.addToDo(todo);
-    setTodo("");}
+    if (todo !== "") {
+      props.addToDo(todo);
+      setTodo("");
+    }
   };
 
-  const updateTodo = (e) =>{
+  const updateTodo = (e) => {
     setTodo(e.target.value);
-  }
+  };
 
   const clearTodo = () => {
-    myProps.clear();
-  }
-  const printToDoList = () =>{
-    
-    if (myProps.todoItems.length !== 0){
-      return myProps.todoItems.map((element) => {
-        return <div className="items" key={element.id}>{element.item}</div>;
-      })
-    }
-  }
-  
+    props.clear();
+  };
+
+
   return (
     <div className="container">
       <h1>TODO APP</h1>
       <div className="inputDiv">
         <span>
-          <input
-            type="text"
-            value={todo}
-            onChange={updateTodo}
-          />
+          <input type="text" value={todo} onChange={updateTodo} />
         </span>
         <span>
-          <input type="submit" value="ADD" onClick={addTodo}/>
-        
+          <input type="submit" value="ADD" onClick={addTodo} />
         </span>
         <span>
-          <input type="submit" value="Clear" onClick={clearTodo}/>
-        
+          <input type="submit" value="Clear" onClick={clearTodo} />
         </span>
       </div>
       <div className="list">
-        {printToDoList()}
+        { props.todoItems !== 0 ? (props.todoItems.map((element) => (
+          <div className="items" key={element.id}>
+            {element.item}
+          </div>
+        ))) : null}
       </div>
     </div>
   );
-})
+});
 
 export default AddToDo;
